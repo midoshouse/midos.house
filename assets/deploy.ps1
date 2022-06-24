ed0aab11a81c863c53a6e3d3a75912a51bc8f9fc
Fenhl <fenhl@fenhl.net>
2022-06-24 07:15:51 +0000

    Add deploy script

---
+++ assets/deploy.ps1
@@ -0,0 +1,15 @@
+function ThrowOnNativeFailure {
+    if (-not $?)
+    {
+        throw 'Native Failure'
+    }
+}
+
+ssh midos.house env -C /opt/git/github.com/midoshouse/midos.house/master git pull
+ThrowOnNativeFailure
+
+ssh midos.house env -C /opt/git/github.com/midoshouse/midos.house/master cargo build --release
+ThrowOnNativeFailure
+
+ssh midos.house sudo systemctl restart midos-house
+ThrowOnNativeFailure
