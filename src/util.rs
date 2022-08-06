ba81cf331e2359ca0afacf4cac145da89378b639
Fenhl <fenhl@fenhl.net>
2022-08-06 07:52:17 +0000

    Use SVG version of racetime.gg favicon

--- src/util.rs
+++ src/util.rs
@@ -263,7 +263,7 @@ pub(crate) fn favicon(url: &Url) -> RawHtml<String> {
             img(class = "favicon", alt = "external link (youtube.com)", srcset = "https://www.youtube.com/s/desktop/435d54f2/img/favicon.ico 16w, https://www.youtube.com/s/desktop/435d54f2/img/favicon_32x32.png 32w, https://www.youtube.com/s/desktop/435d54f2/img/favicon_48x48.png 48w, https://www.youtube.com/s/desktop/435d54f2/img/favicon_96x96.png 96w, https://www.youtube.com/s/desktop/435d54f2/img/favicon_144x144.png 144w");
         },
         Some("racetime.gg") => html! {
-            img(class = "favicon", alt = "external link (racetime.gg)", src = "https://racetime.gg/favicon.ico");
+            img(class = "favicon", alt = "external link (racetime.gg)", src = "/static/racetimeGG-favicon.svg");
         },
         Some("start.gg" | "www.start.gg") => html! {
             img(class = "favicon", alt = "external link (start.gg)", src = "https://www.start.gg/__static/images/favicon/favicon.ico");
