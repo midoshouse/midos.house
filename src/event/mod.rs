6d5969d7b5cb5a8147b630d3b9ebe9ca152fb4e7
Fenhl <fenhl@fenhl.net>
2022-11-15 04:44:42 +0000

    rsl/3: Fix logo weights

--- src/event/mod.rs
+++ src/event/mod.rs
@@ -329,7 +329,12 @@ impl<'a> Data<'a> {
             (Series::Multiworld, _) => unimplemented!(),
             (Series::NineDaysOfSaws, _) => ChestAppearances::VANILLA, // no CAMC in SAWS
             (Series::Pictionary, _) => ChestAppearances::VANILLA, // no CAMC in Pictionary
-            (Series::Rsl, "2" | "3") => ChestAppearances::random(), //TODO
+            (Series::Rsl, "2") => ChestAppearances::random(), //TODO
+            (Series::Rsl, "3") => {
+                static WEIGHTS: Lazy<Vec<(ChestAppearances, usize)>> = Lazy::new(|| serde_json::from_str(include_str!("../../assets/event/rsl/chests-3-a0f568b.json")).expect("failed to parse chest weights"));
+
+                WEIGHTS.choose_weighted(&mut thread_rng(), |(_, weight)| *weight).expect("failed to choose random chest textures").0
+            }
             (Series::Rsl, "4") => {
                 static WEIGHTS: Lazy<Vec<(ChestAppearances, usize)>> = Lazy::new(|| serde_json::from_str(include_str!("../../assets/event/rsl/chests-4-da4dae5.json")).expect("failed to parse chest weights"));
 
