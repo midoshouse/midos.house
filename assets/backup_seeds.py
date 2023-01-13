1f3edb68f37098a3c363829c71e97bbc69e8a381
Fenhl <fenhl@fenhl.net>
2023-01-13 06:11:00 +0000

    backup-seeds: Don't ask for creation timestamp and file hash if they're not used

--- assets/backup_seeds.py
+++ assets/backup_seeds.py
@@ -24,25 +24,25 @@ def b(seed_id, room=None, *, startgg=None, async_room1=None, async_room2=None, u
         file_stem = re.fullmatch('attachment; filename=(.*)\\.zpfz?', patch_resp.headers['Content-Disposition']).group(1)
         with open(SEEDS_DIR / re.fullmatch('attachment; filename=(.*\\.zpfz?)', patch_resp.headers['Content-Disposition']).group(1), 'wb') as patch_f:
             patch_f.write(patch_resp.content)
-    api_resp = requests.get('https://ootrandomizer.com/api/v2/seed/details', params={'id': seed_id, 'key': config['ootrApiKey']})
-    try:
-        api_resp.raise_for_status()
-    except requests.HTTPError:
-        creation_timestamp = f"{datetime.datetime.strptime(input('creation timestamp: ').strip(), '%m/%d/%Y, %I:%M:%S %p UTC'):%Y-%m-%dT%H:%M:%SZ}"
-        file_hash = json.loads(input('file hash: '))
-    else:
-        if api_resp.json()['spoilerLog'] is None and unlock:
-            requests.post('https://ootrandomizer.com/api/v2/seed/unlock', params={'key': config['ootrApiKey'], 'id': seed_id}).raise_for_status()
-            api_resp = requests.get('https://ootrandomizer.com/api/v2/seed/details', params={'id': seed_id, 'key': config['ootrApiKey']})
+    if room is not None or startgg is not None or async_room1 is not None or async_room2 is not None:
+        api_resp = requests.get('https://ootrandomizer.com/api/v2/seed/details', params={'id': seed_id, 'key': config['ootrApiKey']})
+        try:
             api_resp.raise_for_status()
-        creation_timestamp = api_resp.json()['creationTimestamp']
-        if api_resp.json()['spoilerLog'] is None:
-            file_hash = None
+        except requests.HTTPError:
+            creation_timestamp = f"{datetime.datetime.strptime(input('creation timestamp: ').strip(), '%m/%d/%Y, %I:%M:%S %p UTC'):%Y-%m-%dT%H:%M:%SZ}"
+            file_hash = json.loads(input('file hash: '))
         else:
-            with open(SEEDS_DIR / f'{file_stem}_Spoiler.json', 'w') as spoiler_f:
-                spoiler_f.write(api_resp.json()['spoilerLog'])
-            file_hash = json.loads(api_resp.json()['spoilerLog'])['file_hash']
-
+            if api_resp.json()['spoilerLog'] is None and unlock:
+                requests.post('https://ootrandomizer.com/api/v2/seed/unlock', params={'key': config['ootrApiKey'], 'id': seed_id}).raise_for_status()
+                api_resp = requests.get('https://ootrandomizer.com/api/v2/seed/details', params={'id': seed_id, 'key': config['ootrApiKey']})
+                api_resp.raise_for_status()
+            creation_timestamp = api_resp.json()['creationTimestamp']
+            if api_resp.json()['spoilerLog'] is None:
+                file_hash = None
+            else:
+                with open(SEEDS_DIR / f'{file_stem}_Spoiler.json', 'w') as spoiler_f:
+                    spoiler_f.write(api_resp.json()['spoilerLog'])
+                file_hash = json.loads(api_resp.json()['spoilerLog'])['file_hash']
     with conn.cursor() as cur:
         try:
             if room is not None:
