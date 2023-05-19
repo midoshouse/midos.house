90841110da3b294595f8a1709e398d390d5a9667
Fenhl <fenhl@fenhl.net>
2023-05-19 16:34:39 +0000

    Fix base path for rando output files

--- src/racetime_bot.rs
+++ src/racetime_bot.rs
@@ -820,8 +820,8 @@ async fn roll_seed_locally(version: rando::Version, mut settings: serde_json::Ma
         };
         let world_count = settings.get("world_count").map_or(1, |world_count| world_count.as_u64().expect("world_count setting wasn't valid u64").try_into().expect("too many worlds"));
         let patch_path_prefix = if world_count > 1 { "Created patch file archive at: " } else { "Creating Patch File: " };
-        let patch_path = rando_path.join(stderr.iter().rev().find_map(|line| line.strip_prefix(patch_path_prefix)).ok_or(RollError::PatchPath)?);
-        let spoiler_log_path = rando_path.join(stderr.iter().rev().find_map(|line| line.strip_prefix("Created spoiler log at: ")).ok_or(RollError::SpoilerLogPath)?);
+        let patch_path = rando_path.join("Output").join(stderr.iter().rev().find_map(|line| line.strip_prefix(patch_path_prefix)).ok_or(RollError::PatchPath)?);
+        let spoiler_log_path = rando_path.join("Output").join(stderr.iter().rev().find_map(|line| line.strip_prefix("Created spoiler log at: ")).ok_or(RollError::SpoilerLogPath)?);
         let patch_filename = patch_path.file_name().expect("patch file path with no file name");
         fs::rename(&patch_path, Path::new(seed::DIR).join(patch_filename)).await?;
         return Ok((
