0ce1526b58f20395eb65dfdee6495f81254e175d
Fenhl <fenhl@fenhl.net>
2023-08-25 15:46:14 +0000

    Use JS formatRange method instead of formatting date ranges manually

--- assets/static/common.js
+++ assets/static/common.js
@@ -9,11 +9,5 @@ document.querySelectorAll('.datetime').forEach(function(dateTime) {
 document.querySelectorAll('.daterange').forEach(function(dateRange) {
     var start = new Date(parseInt(dateRange.dataset.start));
     var end = new Date(parseInt(dateRange.dataset.end));
-    if (start.getFullYear() != end.getFullYear()) {
-        dateRange.textContent = start.toLocaleString([], {dateStyle: 'long'}) + '–' + end.toLocaleString([], {dateStyle: 'long'});
-    } else if (start.getDate() != end.getDate()) {
-        dateRange.textContent = start.toLocaleString([], {month: 'long', day: 'numeric'}) + '–' + end.toLocaleString([], {dateStyle: 'long'});
-    } else {
-        dateRange.textContent = start.toLocaleString([], {dateStyle: 'long'});
-    }
+    dateRange.textContent = Intl.DateTimeFormat([], {dateStyle: 'long'}).formatRange(start, end);
 });
