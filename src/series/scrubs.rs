ef4b51d153e4576d9bb36d01d979d34cf159be55
Fenhl <fenhl@fenhl.net>
2023-10-19 11:29:39 +0000

    scrubs/5: Add links to qualifier standings and brackets

--- src/series/scrubs.rs
+++ src/series/scrubs.rs
@@ -17,6 +17,30 @@ pub(crate) async fn info(transaction: &mut Transaction<'_, Postgres>, data: &Dat
                     a(href = "https://discord.gg/hpHngEY") : "Join the Discord server";
                     : " for details.";
                 }
+                h2 : "See also";
+                ul {
+                    li {
+                        a(href = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRtASXFkNaSzqJoFSmjDpU2XfClRdogkRAgTsJ7RSCiZwUwkrXNcjF06fO_I8vMWfchkUKCrACXPmyE/pubhtml?gid=566134238") : "Qualifier scores & offline qualifier times";
+                    }
+                    li {
+                        a(href = "https://scrubscentral.challonge.com/ootrs5minuet") : "Minuet brackets";
+                    }
+                    li {
+                        a(href = "https://scrubscentral.challonge.com/ootrs5bolero") : "Bolero brackets";
+                    }
+                    li {
+                        a(href = "https://scrubscentral.challonge.com/ootrs5serenade") : "Serenade brackets";
+                    }
+                    li {
+                        a(href = "https://scrubscentral.challonge.com/ootrs5nocturne") : "Nocturne brackets";
+                    }
+                    li {
+                        a(href = "https://scrubscentral.challonge.com/ootrs5requiem") : "Requiem brackets";
+                    }
+                    li {
+                        a(href = "https://scrubscentral.challonge.com/ootrs5prelude") : "Prelude brackets";
+                    }
+                }
             }
         }),
         _ => None,
