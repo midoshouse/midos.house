b4499a1569c6374e8ac91461427477f957957b1f
Fenhl <fenhl@fenhl.net>
2023-10-23 08:07:55 +0000

    Mark Dev 7.1.199 as a known good web version

--- src/racetime_bot.rs
+++ src/racetime_bot.rs
@@ -62,12 +62,13 @@ use {
 pub(crate) const CATEGORY: &str = "ootr";
 
 /// Randomizer versions that are known to exist on the ootrandomizer.com API. Hardcoded because the API doesn't have a “does version x exist?” endpoint.
-const KNOWN_GOOD_WEB_VERSIONS: [rando::Version; 9] = [
+const KNOWN_GOOD_WEB_VERSIONS: [rando::Version; 10] = [
     rando::Version::from_dev(6, 2, 181),
     rando::Version::from_dev(6, 2, 205),
     rando::Version::from_dev(7, 1, 143),
     rando::Version::from_dev(7, 1, 181),
     rando::Version::from_dev(7, 1, 191),
+    rando::Version::from_dev(7, 1, 199),
     rando::Version::from_branch(rando::Branch::DevR, 6, 2, 238, 1),
     rando::Version::from_branch(rando::Branch::DevR, 7, 1, 83, 1), // commit 578a64f4c78a831cde4215e0ac31565d3bf9bc46
     rando::Version::from_branch(rando::Branch::DevR, 7, 1, 143, 1), // commit 06390ece7e38fce1dd02ca60a28a7b1ff9fceb10
