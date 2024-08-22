19602402911a4762c4abc2156c7b1d30b9e3d440
Fenhl <fenhl@fenhl.net>
2024-08-22 14:06:01 +0000

    s/w: Update with new settings

--- src/event/mod.rs
+++ src/event/mod.rs
@@ -298,7 +298,7 @@ impl<'a> Data<'a> {
             (Series::SongsOfHope, "1") => from_file!("../../assets/event/soh/chests-1-8.1.json"),
             (Series::SpeedGaming, "2023onl" | "2023live") => from_file!("../../assets/event/sgl/chests-2023-42da4aa.json"),
             (Series::SpeedGaming, "2024onl" | "2024live") => from_file!("../../assets/event/sgl/chests-2024-ee4d35b.json"),
-            (Series::Standard, "w") => from_file!("../../assets/event/scrubs/chests-6-8.1.73.json"),
+            (Series::Standard, "w") => from_file!("../../assets/event/s/chests-w-8.1.77.json"),
             (Series::Standard, "6") => from_file!("../../assets/event/s/chests-6-6.9.10.json"),
             (Series::Standard, "7" | "7cc") => from_file!("../../assets/event/s/chests-7-7.1.198.json"),
             (Series::TournoiFrancophone, "3") => from_file!("../../assets/event/fr/chests-3-7.1.83-r.1.json"),
