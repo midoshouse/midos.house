afb9784b26c18ba5843bd3715f824da9165bf821
Fenhl <fenhl@fenhl.net>
2024-09-21 16:53:36 +0000

    s/w: Update settings

--- src/series/s.rs
+++ src/series/s.rs
@@ -316,7 +316,7 @@ pub(crate) fn weeklies_enter_form(me: Option<&User>) -> RawHtml<String> {
 
 pub(crate) fn weekly_settings_2024w38() -> serde_json::Map<String, Json> {
     collect![
-        format!("user_message") => json!("Standard Weekly (2024-09-20)"),
+        format!("user_message") => json!("Standard Weekly (2024-09-21)"),
         format!("bridge") => json!("vanilla"),
         format!("trials") => json!(0),
         format!("shuffle_ganon_bosskey") => json!("medallions"),
