fcdb5e3a8e693318f7ab83dab445c637d9bdd970
Fenhl <fenhl@fenhl.net>
2025-03-30 08:38:25 +0000

    Replace git2 build dependency with gix

--- build.rs
+++ build.rs
@@ -12,38 +12,44 @@ use {
             PathBuf,
         },
     },
-    git2::{
-        Oid,
+    gix::{
+        ObjectId,
         Repository,
     },
 };
 
 #[derive(Debug, thiserror::Error)]
 enum Error {
-    #[error(transparent)] Git(#[from] git2::Error),
+    #[error(transparent)] GitCommit(#[from] gix::object::commit::Error),
+    #[error(transparent)] GitDiff(#[from] gix::repository::diff_tree_to_tree::Error),
+    #[error(transparent)] GitFind(#[from] gix::object::find::existing::Error),
+    #[error(transparent)] GitHeadCommit(#[from] gix::reference::head_commit::Error),
+    #[error(transparent)] GitOpen(#[from] gix::open::Error),
+    #[error(transparent)] GitPeel(#[from] gix::object::peel::to_kind::Error),
     #[error(transparent)] Io(#[from] std::io::Error),
 }
 
-fn check_static_file(cache: &mut HashMap<PathBuf, Oid>, repo: &Repository, relative_path: &Path, path: PathBuf) -> Result<(), Error> {
-    let mut iter_commit = repo.head()?.peel_to_commit()?;
+fn check_static_file(cache: &mut HashMap<PathBuf, ObjectId>, repo: &Repository, relative_path: &Path, path: PathBuf) -> Result<(), Error> {
+    let mut iter_commit = repo.head_commit()?;
     let commit_id = loop {
         let iter_commit_id = iter_commit.id();
-        if iter_commit.parent_count() != 1 {
+        let parents = iter_commit.parent_ids().collect::<Vec<_>>();
+        let [parent] = &*parents else {
             // initial commit or merge commit; mark the file as updated here for simplicity's sake
             break iter_commit_id
-        }
-        let parent = iter_commit.parent(0)?;
-        let diff = repo.diff_tree_to_tree(Some(&parent.tree()?), Some(&iter_commit.tree()?), Some(git2::DiffOptions::default().pathspec(&path)))?;
-        if diff.deltas().next().is_some() {
+        };
+        let parent = parent.object()?.peel_to_commit()?;
+        let diff = repo.diff_tree_to_tree(&parent.tree()?, &iter_commit.tree()?, None)?;
+        if path.to_str().is_none_or(|path| diff.into_iter().any(|change| change.location() == path)) {
             break iter_commit_id
         }
         iter_commit = parent;
     };
-    cache.insert(relative_path.to_owned(), commit_id);
+    cache.insert(relative_path.to_owned(), commit_id.into());
     Ok(())
 }
 
-fn check_static_dir(cache: &mut HashMap<PathBuf, Oid>, repo: &Repository, relative_path: &Path, path: PathBuf) -> Result<(), Error> {
+fn check_static_dir(cache: &mut HashMap<PathBuf, ObjectId>, repo: &Repository, relative_path: &Path, path: PathBuf) -> Result<(), Error> {
     for entry in fs::read_dir(&path)? {
         let entry = entry?;
         if entry.file_type()?.is_dir() {
@@ -59,7 +65,7 @@ fn main() -> Result<(), Error> {
     println!("cargo:rerun-if-changed=nonexistent.foo"); // check a nonexistent file to make sure build script is always run (see https://github.com/rust-lang/cargo/issues/4213 and https://github.com/rust-lang/cargo/issues/5663)
     let static_dir = Path::new("assets").join("static");
     let mut cache = HashMap::default();
-    let repo = Repository::open(&env::var_os("CARGO_MANIFEST_DIR").unwrap())?;
+    let repo = gix::open(&env::var_os("CARGO_MANIFEST_DIR").unwrap())?;
     for entry in fs::read_dir(&static_dir)? {
         let entry = entry?;
         if entry.file_type()?.is_dir() {
