59e5a5a3f8e31c4912bb8acb7b1961a882bf3305
Fenhl <fenhl@fenhl.net>
2025-05-17 07:24:16 +0000

    tfb: Accept invalid TLS certs

--- src/main.rs
+++ src/main.rs
@@ -199,6 +199,7 @@ async fn main(Args { port, subcommand }: Args) -> Result<(), Error> {
         let insecure_http_client = reqwest::Client::builder()
             .user_agent(concat!("MidosHouse/", env!("CARGO_PKG_VERSION"), " (https://github.com/midoshouse/midos.house)"))
             .timeout(Duration::from_secs(30))
+            .danger_accept_invalid_certs(true) // https://discord.com/channels/274180765816848384/1012773802201071736/1372836620822122526
             .use_rustls_tls()
             .hickory_dns(true)
             .build()?;
