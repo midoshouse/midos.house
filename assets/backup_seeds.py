3e3825d692daeaab079030dd00411a837cd69d9e
Fenhl <fenhl@fenhl.net>
2025-05-19 18:16:15 +0000

    backup_seeds: Allow supplying race_id param

--- assets/backup_seeds.py
+++ assets/backup_seeds.py
@@ -18,7 +18,7 @@ with open('/etc/xdg/midos-house.json') as config_f:
 
 conn = psycopg.connect('dbname=midos_house user=mido')
 
-def b(seed_id, room=None, *, startgg=None, async_room1=None, async_room2=None, async_room3=None, unlock=True):
+def b(seed_id, room=None, *, race_id=None, startgg=None, async_room1=None, async_room2=None, async_room3=None, unlock=True):
     patch_resp = requests.get('https://ootrandomizer.com/patch/get', params={'id': seed_id})
     if patch_resp.status_code == 404:
         file_stem = input('file stem: ').strip()
@@ -31,7 +31,7 @@ def b(seed_id, room=None, *, startgg=None, async_room1=None, async_room2=None, a
     try:
         api_resp.raise_for_status()
     except requests.HTTPError:
-        if room is not None or startgg is not None or async_room1 is not None or async_room2 is not None or async_room3 is not None:
+        if race_id is not None or room is not None or startgg is not None or async_room1 is not None or async_room2 is not None or async_room3 is not None:
             page_resp = requests.get('https://ootrandomizer.com/seed/get', params={'id': seed_id})
             page_resp.raise_for_status()
             soup = bs4.BeautifulSoup(page_resp.text)
@@ -56,6 +56,19 @@ def b(seed_id, room=None, *, startgg=None, async_room1=None, async_room2=None, a
             file_hash = json.loads(api_resp.json()['spoilerLog'])['file_hash']
     with conn.cursor() as cur:
         try:
+            if race_id is not None:
+                if race_id >= 2 ** 63:
+                    race_id -= 2 ** 64
+                cur.execute("""UPDATE races SET
+                    web_id = %s,
+                    web_gen_time = %s,
+                    file_stem = %s,
+                    hash1 = %s,
+                    hash2 = %s,
+                    hash3 = %s,
+                    hash4 = %s,
+                    hash5 = %s
+                WHERE id = %s""", (seed_id, creation_timestamp, file_stem, *file_hash, race_id))
             if room is not None:
                 cur.execute("""UPDATE races SET
                     web_id = %s,
