7a47440d5b3369d80aaa6a30c4203e1a1e66eb4f
Fenhl <fenhl@fenhl.net>
2025-10-13 16:00:53 +0000

    pre-commit: Check .sqlx on Unix

--- assets/pre-commit.py
+++ assets/pre-commit.py
@@ -4,10 +4,10 @@ import re
 import subprocess
 
 subprocess.run(['cargo', 'check'], check=True)
-subprocess.run(['cargo', 'sqlx', 'prepare', '--check'], check=True)
 subprocess.run(['wsl', 'rsync', '--delete', '-av', '/mnt/c/Users/fenhl/git/github.com/midoshouse/midos.house/stage/', '/home/fenhl/wslgit/github.com/midoshouse/midos.house/', '--exclude', 'target'], check=True) # copy the tree to the WSL file system to improve compile times
 subprocess.run(['wsl', 'rustup', 'update', 'stable'], check=True)
 subprocess.run(['wsl', 'env', '-C', '/home/fenhl/wslgit/github.com/midoshouse/midos.house', 'cargo', 'check'], check=True)
+subprocess.run(['wsl', 'env', '-C', '/home/fenhl/wslgit/github.com/midoshouse/midos.house', 'cargo', 'sqlx', 'prepare', '--check'], check=True)
 
 with open('assets/schema.sql', encoding='utf-8') as f: #TODO check staged changes instead of worktree
     prepared_schema = re.sub(r'\\(un)?restrict \w*', r'\\\1restrict NSkHPci93sAFqHtSzSNGsBd7dCxhH7NpHe4WhC8jFzIipftC7A6hpgap0hCfbqM', f.read())
