88544c76e11fc1b07e3fbf313bdb5de9654c53c6
Fenhl <fenhl@fenhl.net>
2026-02-26 09:53:17 +0000

    tot/2: Update settings

--- src/racetime_bot/mod.rs
+++ src/racetime_bot/mod.rs
@@ -597,11 +597,7 @@ impl Goal {
             Self::Mq => VersionedBranch::Pinned { version: rando::Version::from_dev(8, 2, 0) },
             Self::MultiworldS3 => VersionedBranch::Pinned { version: rando::Version::from_dev(6, 2, 205) },
             Self::MultiworldS4 => VersionedBranch::Pinned { version: rando::Version::from_dev(7, 1, 199) },
-            Self::MultiworldS5 => VersionedBranch::Pinned { version: if Utc::now() >= Utc.with_ymd_and_hms(2025, 6, 4, 16, 0, 0).single().expect("wrong hardcoded datetime") {
-                rando::Version::from_dev(8, 3, 0)
-            } else {
-                rando::Version::from_dev(8, 2, 76)
-            } },
+            Self::MultiworldS5 => VersionedBranch::Pinned { version: rando::Version::from_dev(8, 3, 0) },
             Self::NineDaysOfSaws => VersionedBranch::Pinned { version: rando::Version::from_branch(rando::Branch::DevFenhl, 6, 9, 14, 2) },
             Self::Pic7 => VersionedBranch::Custom { github_username: Cow::Borrowed("fenhl"), branch: Cow::Borrowed("frogs2-melody") },
             Self::S6 => VersionedBranch::Pinned { version: rando::Version::from_dev(7, 1, 0) },
@@ -622,7 +618,7 @@ impl Goal {
                 }
             },
             Self::StandardWeeklies => event.expect("event must be set for Goal::StandardWeeklies").rando_version.clone().expect("no randomizer version configured for weeklies"), //TODO allow weekly organizers to configure this
-            Self::TournamentOfTruthS2 => VersionedBranch::Pinned { version: rando::Version::from_branch(rando::Branch::DevRob, 9, 0, 2, 17) },
+            Self::TournamentOfTruthS2 => VersionedBranch::Pinned { version: rando::Version::from_dev(9, 0, 0) },
             Self::TournoiFrancoS3 => VersionedBranch::Pinned { version: rando::Version::from_branch(rando::Branch::DevR, 7, 1, 143, 1) },
             Self::TournoiFrancoS4 => VersionedBranch::Pinned { version: rando::Version::from_branch(rando::Branch::DevRob, 8, 1, 45, 105) },
             Self::TournoiFrancoS5 => VersionedBranch::Pinned { version: rando::Version::from_branch(rando::Branch::DevRob, 8, 2, 64, 135) },
